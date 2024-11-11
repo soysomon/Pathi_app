3b65d927092c3888346df6bf353df106a7279ccb
Elian <20222035@itla.edu.do>
2024-11-11 19:22:45 -0400

    Añadiendo soporte para la gestión de temas y rutas de navegación en la aplicación

--- frontend/app_turistica_front/lib/main.dart
+++ frontend/app_turistica_front/lib/main.dart
@@ -1,27 +1,54 @@
-import 'package:app_turistica_front/screens/home_screen.dart';
+import 'package:app_turistica_front/screens/helpcenter/help_center.dart';
+import 'package:app_turistica_front/screens/profile/profile_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../screens/home/home_screen.dart';
+import 'package:app_turistica_front/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
+import 'services/theme_service.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? token = prefs.getString('jwt_token');
+
+  if (token == null) {
+    await prefs.clear();
+  }
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeService(),
+      child: MyApp(initialRoute: token == null ? '/login' : '/home'),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String initialRoute;
+
+  const MyApp({Key? key, required this.initialRoute}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final themeService = Provider.of<ThemeService>(context);
+
     return MaterialApp(
       title: 'App Turística',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
+      theme: themeService.currentTheme,
+      initialRoute: initialRoute,
       routes: {
         '/': (context) => const WelcomeScreen(),
         '/login': (context) => const LoginScreen(),
-        '/home': (context) => const HomeScreen(),  // Ruta para la pantalla de inicio (después del login)
+        '/home': (context) => const HomeScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/profile': (context) => const ProfileScreen(),  
+        '/helpcenter': (context) => const HelpCenter(),
       },
     );
   }
