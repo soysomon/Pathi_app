113333bff2e439980f7796e85e12a2ffc81612e1
Elian <20222035@itla.edu.do>
2024-11-25 03:28:52 -0400

    Add navigation to DestinationsScreen with slide transition in SectionTitle component

--- frontend/app_turistica_front/lib/screens/home/components/section_title.dart
+++ frontend/app_turistica_front/lib/screens/home/components/section_title.dart
@@ -1,3 +1,4 @@
+import 'package:app_turistica_front/screens/destinations/destinations_screen.dart';
 import 'package:flutter/material.dart';
 
 class SectionTitle extends StatelessWidget {
@@ -24,7 +25,25 @@ class SectionTitle extends StatelessWidget {
           ),
         ),
         TextButton(
-          onPressed: press,
+          onPressed: () {
+            Navigator.of(context).push(
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) => DestinationsScreen(),
+                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                  const begin = Offset(0.0, 1.0);
+                  const end = Offset.zero;
+                  const curve = Curves.ease;
+
+                  var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+                  return SlideTransition(
+                    position: animation.drive(tween),
+                    child: child,
+                  );
+                },
+              ),
+            );
+          },
           style: TextButton.styleFrom(foregroundColor: Colors.grey),
           child: const Text("See more"),
         ),
