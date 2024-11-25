182850db6fbdaaa1e8a16880f71dcfcf823aba3a
Elian <20222035@itla.edu.do>
2024-11-25 03:28:06 -0400

    Implement search functionality in SearchField to fetch and display destination results

--- frontend/app_turistica_front/lib/screens/destinations/components/search_field.dart
+++ frontend/app_turistica_front/lib/screens/destinations/components/search_field.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../constants.dart';
 
 class SearchField extends StatelessWidget {
+  final Function(List<dynamic>) onSearchResults;
+
   const SearchField({
     Key? key,
+    required this.onSearchResults,
   }) : super(key: key);
+
+  Future<void> searchDestinations(String query) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt_token') ?? '';
+
+    final baseUrl = dotenv.env['API_BASE_URL'];
+    final url = Uri.parse('$baseUrl/buscar-destinos?search=$query');
+    final response = await http.get(url, headers: {
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode == 200) {
+      final List<dynamic> destinations = json.decode(response.body);
+      onSearchResults(destinations); // Llama al callback con los resultados
+    } else {
+      print('Error: ${response.statusCode}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
       child: TextFormField(
-        onChanged: (value) {},
+        onChanged: (value) {
+          if (value.isNotEmpty) {
+            searchDestinations(value);
+          } else {
+            onSearchResults([]); // Limpia los resultados si el campo está vacío
+          }
+        },
         decoration: InputDecoration(
           filled: true,
           fillColor: kSecondaryColor.withOpacity(0.1),
