28c3c871db022fe2d82b68004c3e4ea4c4234b7a
Elian <20222035@itla.edu.do>
2024-11-25 03:29:58 -0400

    Add profile image support to PromotionsHeader component; integrate ProfileImageService for dynamic image loading

--- frontend/app_turistica_front/lib/screens/promotions/components/promotions_header.dart
+++ frontend/app_turistica_front/lib/screens/promotions/components/promotions_header.dart
@@ -1,14 +1,20 @@
 import 'package:app_turistica_front/screens/profile/profile_screen.dart';
-import 'package:app_turistica_front/screens/home/components/icon_btn_with_counter.dart';
+import 'package:app_turistica_front/services/profile_image_service.dart';
 import 'package:flutter/material.dart';
+import 'package:app_turistica_front/screens/home/components/icon_btn_with_counter.dart';
+import 'package:provider/provider.dart';
 
 class PromotionsHeader extends StatelessWidget {
   const PromotionsHeader({
     Key? key,
+    this.profileImage,
   }) : super(key: key);
+
+  final String? profileImage;
 
   @override
   Widget build(BuildContext context) {
+    final profileImageService = Provider.of<ProfileImageService>(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Row(
@@ -23,8 +29,9 @@ class PromotionsHeader extends StatelessWidget {
           ),
           const SizedBox(width: 16),
           IconBtnWithCounter(
-            svgSrc: "assets/icons/User Icon.svg",
             press: () => Navigator.pushNamed(context, ProfileScreen.routeName),
+            profileImage: profileImageService.imageUrl,
+
           ),
         ],
       ),
