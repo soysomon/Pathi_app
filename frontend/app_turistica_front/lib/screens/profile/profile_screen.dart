4f9f097877ded29074574387ee53de8356396681
Elian <20222035@itla.edu.do>
2024-11-25 03:29:11 -0400

    Refactor ProfileScreen to load user profile data, manage profile image URL, and integrate public status; enhance image handling and loading states

--- frontend/app_turistica_front/lib/screens/profile/profile_screen.dart
+++ frontend/app_turistica_front/lib/screens/profile/profile_screen.dart
@@ -1,11 +1,11 @@
 import 'package:app_turistica_front/screens/helpcenter/help_center.dart';
 import 'package:app_turistica_front/screens/home/home_screen.dart';
+import 'package:app_turistica_front/services/profile_image_service.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:provider/provider.dart';
-import '/services/theme_service.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:io';
@@ -16,7 +16,8 @@ import 'package:app_turistica_front/screens/profile/components/profile_pic.dart'
 import 'package:app_turistica_front/screens/profile/components/publish_services.dart';
 import 'package:app_turistica_front/screens/profile/components/publish_profile.dart';
 import 'package:app_turistica_front/screens/profile/components/publish_promotion.dart';
-import 'package:app_turistica_front/screens/profile/components/dark_mode.dart';
+import 'package:app_turistica_front/screens/profile/components/general_publish.dart';
+import 'package:app_turistica_front/services/publicar_service.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -32,11 +33,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
   String imageUrl = 'assets/avatar.png'; // URL de la imagen de perfil
   File? _imageFile; // Archivo de imagen seleccionado
   bool isDarkMode = false; // Estado del modo oscuro
+  bool _isProfileDataLoaded = false; // Controla si los datos del perfil ya han sido cargados
 
-  final TextEditingController _nombreUsuarioController =
-      TextEditingController();
-  final TextEditingController _emailController =
-      TextEditingController(); // Para editar el email
+  final TextEditingController _nombreUsuarioController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController(); // Para editar el email
+
+  final PublicarService publicarService = PublicarService();
+  bool isPublico = false;
 
   // Función para alternar el modo oscuro
   void _toggleDarkMode(bool value) {
@@ -47,8 +50,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   Future<void> _fetchProfileData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    final token =
-        await prefs.getString('jwt_token'); // Recupera el token guardado
+    final token = await prefs.getString('jwt_token'); // Recupera el token guardado
     if (token != null) {
       try {
         final response = await http.get(
@@ -61,19 +63,27 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
         if (response.statusCode == 200) {
           final data = json.decode(response.body);
+          final userId = data['id']; // Asume que el ID del usuario está en el campo 'id'
+          await prefs.setString('user_id', userId); // Guarda el ID del usuario en las preferencias
           setState(() {
             nombreUsuario = data['nombre_usuario'] ?? '';
             email = data['email'] ?? 'email@dominio.com';
             imageUrl = data['foto_url'] ?? 'assets/avatar.png';
-            _nombreUsuarioController.text =
-                nombreUsuario; // Inicializa campo de texto
+            _nombreUsuarioController.text = nombreUsuario; // Inicializa campo de texto
             _emailController.text = email; // Inicializa el campo de email
+            Provider.of<ProfileImageService>(context, listen: false).setImageUrl(imageUrl);
+            _isProfileDataLoaded = true; // Marca los datos del perfil como cargados
+          });
+
+          // Obtener el estado de "publico"
+          final publicoStatus = await publicarService.getPublicoStatus(userId);
+          setState(() {
+            isPublico = publicoStatus ?? false;
           });
         } else if (response.statusCode == 404) {
           print('Usuario no encontrado');
         } else {
-          print(
-              'Error al obtener los datos del perfil: ${response.statusCode}');
+          print('Error al obtener los datos del perfil: ${response.statusCode}');
         }
       } catch (e) {
         print('Error al realizar la solicitud: $e');
@@ -116,8 +126,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   // Función para seleccionar una imagen desde la galería
   Future<void> _pickImageFromGallery() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       setState(() {
         _imageFile = File(pickedFile.path);
@@ -128,8 +137,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   // Función para abrir la cámara
   Future<void> _openCamera() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.camera);
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
     if (pickedFile != null) {
       setState(() {
         _imageFile = File(pickedFile.path);
@@ -148,8 +156,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         Uri.parse('${dotenv.env['API_BASE_URL']}/perfil/foto'),
       );
       request.headers['Authorization'] = 'Bearer ${await _getToken()}';
-      request.files
-          .add(await http.MultipartFile.fromPath('foto', _imageFile!.path));
+      request.files.add(await http.MultipartFile.fromPath('foto', _imageFile!.path));
 
       final response = await request.send();
 
@@ -184,7 +191,20 @@ class _ProfileScreenState extends State<ProfileScreen> {
     if (_imageFile != null && _imageFile!.path.isNotEmpty) {
       return _imageFile!.path;
     }
-    return imageUrl.isNotEmpty ? imageUrl : 'assets/avatar.png';
+    if (imageUrl.isNotEmpty) {
+      // Reemplaza 'localhost' con la URL base del API si está presente
+      if (imageUrl.contains('localhost')) {
+        final String apiUrl = dotenv.env['API_BASE_URL']!;
+        return imageUrl.replaceFirst('http://localhost:3000', apiUrl);
+      } else if (imageUrl.startsWith('http')) {
+        return imageUrl;
+      } else {
+        final String apiUrl = dotenv.env['API_BASE_URL']!;
+        return '$apiUrl/$imageUrl';
+      }
+    }
+    imageUrl = 'assets/avatar.png';
+    return imageUrl;
   }
 
   Future<void> _updateProfile() async {
@@ -200,8 +220,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         'Content-Type': 'application/json'
       },
       body: json.encode({
-        'nombre_usuario':
-            _nombreUsuarioController.text, // Siempre enviar el valor actual
+        'nombre_usuario': _nombreUsuarioController.text, // Siempre enviar el valor actual
         'email': _emailController.text, // Siempre enviar el valor actual
       }),
     );
@@ -213,6 +232,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           email = _emailController.text;
         });
         Navigator.of(context).pop(); // Cerrar el diálogo
+        _fetchProfileData(); // Refrescar los datos del perfil
       }
     } else {
       print('Error al actualizar el perfil');
@@ -222,13 +242,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   void initState() {
     super.initState();
-    _fetchProfileData(); // Cargar los datos del perfil al iniciar la pantalla
+    if (!_isProfileDataLoaded) {
+      _fetchProfileData(); // Cargar los datos del perfil al iniciar la pantalla
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final themeService = Provider.of<ThemeService>(context);
-
     return Scaffold(
       backgroundColor: Colors.white, // Fondo blanco
       appBar: AppBar(
@@ -246,8 +266,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             Navigator.pushReplacement(
               context,
               MaterialPageRoute(
-                  builder: (context) =>
-                      HomeScreen()), // Regresa a HomeScreen, no al login
+                  builder: (context) => HomeScreen()), // Regresa a HomeScreen, no al login
             );
           },
         ),
@@ -276,10 +295,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 },
               ),
               const SizedBox(height: 20),
-              DarkModeSwitch(
-                isDarkMode: themeService.isDarkMode,
-                onToggle: (value) {
-                  themeService.toggleTheme();
+              FutureBuilder<String?>(
+                future: SharedPreferences.getInstance().then((prefs) => prefs.getString('user_id')),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return MostrarCompaniaSwitch(
+                      isPublico: isPublico,
+                      userId: snapshot.data ?? '',
+                    );
+                  }
                 },
               ),
               ProfileMenu(
@@ -329,12 +357,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 text: "Log Out",
                 icon: "assets/icons/Log out.svg",
                 press: () async {
+                  Provider.of<ProfileImageService>(context, listen: false).clearImageUrl();
                   await logout();
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(
-                        builder: (context) =>
-                            LoginScreen()), // Regresa a HomeScreen, no al login
+                        builder: (context) => LoginScreen()), // Regresa a HomeScreen, no al login
                   );
                 },
               ),
