087adfd92a2dce4ca3b690d9e7fe639b0bc98e63
Elian <20222035@itla.edu.do>
2024-11-25 03:27:46 -0400

    Refactor DestinationsHeader to integrate profile image service and enhance search functionality

--- frontend/app_turistica_front/lib/screens/destinations/components/destinations_header.dart
+++ frontend/app_turistica_front/lib/screens/destinations/components/destinations_header.dart
@@ -1,25 +1,39 @@
-import 'package:app_turistica_front/screens/destinations/components/icon_btn_with_counter.dart';
+import 'package:app_turistica_front/screens/home/components/icon_btn_with_counter.dart';
 import 'package:app_turistica_front/screens/profile/profile_screen.dart';
+import 'package:app_turistica_front/services/profile_image_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'search_field.dart';
 
 class DestinationsHeader extends StatelessWidget {
+
+  final Function(List<dynamic>) onSearchResults;
+
   const DestinationsHeader({
     Key? key,
+    this.profileImage,
+    required this.onSearchResults,
   }) : super(key: key);
+
+  final String? profileImage;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    final profileImageService = Provider.of<ProfileImageService>(context);
+    return Container(
+      margin: const EdgeInsets.only(bottom: 9), // Margen inferior de 5px
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Expanded(child: SearchField()),
+          Expanded(child: SearchField(onSearchResults: onSearchResults)),
           const SizedBox(width: 16),
-          IconBtnWithCounter(
-            svgSrc: "assets/icons/User Icon.svg",
-            press: () => Navigator.pushNamed(context, ProfileScreen.routeName),
+          Container(
+            margin: const EdgeInsets.only(bottom: 7), // Margen inferior de 5px
+            child: IconBtnWithCounter(
+              press: () => Navigator.pushNamed(context, ProfileScreen.routeName),
+              profileImage: profileImageService.imageUrl,
+            ),
           ),
         ],
       ),
