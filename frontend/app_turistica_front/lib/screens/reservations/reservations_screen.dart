dc6d40bb37bb434d8202e4eb1a2505fb4f1ee9b1
Elian <20222035@itla.edu.do>
2024-11-25 03:30:04 -0400

    Implement reservations fetching and cancellation functionality; add loading state and skeleton UI for better user experience

--- frontend/app_turistica_front/lib/screens/reservations/reservations_screen.dart
+++ frontend/app_turistica_front/lib/screens/reservations/reservations_screen.dart
@@ -1,13 +1,17 @@
-import 'package:app_turistica_front/screens/reservations/components/reservation_header.dart';
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import '../reservations/components/reservation_card.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:app_turistica_front/screens/reservations/components/reservation_header.dart';
+import 'package:app_turistica_front/screens/reservations/components/reservation_card.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ReservationScreen extends StatelessWidget {
   const ReservationScreen({Key? key}) : super(key: key);
   static String routeName = "/reservations";
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ReservationContent(),
@@ -15,34 +19,105 @@ class ReservationScreen extends StatelessWidget {
   }
 }
 
-class ReservationsList extends StatelessWidget {
-  const ReservationsList({
-    Key? key,
-  }) : super(key: key);
+class ReservationsList extends StatefulWidget {
+  const ReservationsList({Key? key}) : super(key: key);
 
-  final List<Map<String, dynamic>> reservations = const [
-    {
-      "image": "assets/images/Image Banner 2.png",
-      "name": "Restaurante El Buen Sabor",
-      "rating": 4.5,
-      "location": "Calle 123, Ciudad",
-      "date": "2023-10-01",
-      "time": "19:00",
-    },
-    {
-      "image": "assets/images/Image Banner 3.png",
-      "name": "Restaurante La Buena Mesa",
-      "rating": 4.0,
-      "location": "Avenida 456, Ciudad",
-      "date": "2023-10-02",
-      "time": "20:00",
-    },
-    // Agrega más reservas aquí
-  ];           
+  @override
+  _ReservationsListState createState() => _ReservationsListState();
+}
 
+class _ReservationsListState extends State<ReservationsList> {
+  List<dynamic> reservations = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchReservations();
+  }
+
+  Future<void> fetchReservations() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt_token');
+    if (token == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Token no disponible, inicia sesión nuevamente')),
+      );
+      return;
+    }
+
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_BASE_URL']}/listar_reservas'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        reservations = json.decode(response.body);
+        isLoading = false;
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al obtener las reservas')),
+      );
+    }
+  }
+
+  Future<void> cancelReservation(int reservationId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt_token');
+    if (token == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Token no disponible, inicia sesión nuevamente')),
+      );
+      return;
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse('${dotenv.env['API_BASE_URL']}/refund'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: json.encode({
+          'reservationId': reservationId,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Reserva cancelada y reembolso realizado correctamente')),
+        );
+        fetchReservations(); // Refrescar la lista de reservas
+      } else {
+        print('Error al cancelar la reserva: ${response.body}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error al cancelar la reserva: ${response.body}')),
+        );
+      }
+    } catch (e) {
+      print('Exception al cancelar la reserva: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Exception al cancelar la reserva: $e')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (isLoading) {
+      return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: 4, // Número de esqueletos a mostrar
+        itemBuilder: (context, index) => SkeletonReservationCard(),
+      );
+    }
+
     return ListView.builder(
       padding: const EdgeInsets.all(16),
       shrinkWrap: true,
@@ -51,14 +126,70 @@ class ReservationsList extends StatelessWidget {
       itemBuilder: (context, index) {
         final reservation = reservations[index];
         return ReservationCard(
-          image: reservation['image'],
-          name: reservation['name'],
-          rating: reservation['rating'],
-          location: reservation['location'],
-          date: reservation['date'],
-          time: reservation['time'],
+          image: reservation['imagen_empresarial'],
+          reservationName: reservation['nombre_usuario'],
+          reservationEmail: reservation['correo'],
+          reservationPhone: reservation['telefono'],
+          reservationDate: reservation['fecha'],
+          reservationTime: reservation['hora'],
+          totalPayment: reservation['total_pagar'],
+          userName: reservation['nombre_usuario'],
+          userLocation: reservation['ubicacion'],
+          userDetails: reservation['detalles'],
+          onCancel: () => cancelReservation(reservation['id']),
         );
       },
+    );
+  }
+}
+
+class SkeletonReservationCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: SizedBox(
+        width: double.infinity,
+        height: 150,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Shimmer.fromColors(
+            baseColor: Colors.grey[300]!,
+            highlightColor: Colors.grey[100]!,
+            child: Container(
+              color: Colors.grey[300],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 18,
+                      color: Colors.grey[300],
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      width: double.infinity,
+                      height: 14,
+                      color: Colors.grey[300],
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      width: double.infinity,
+                      height: 14,
+                      color: Colors.grey[300],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
